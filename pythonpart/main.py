9ebc1014cca26b2d400768cdd7b3869b41e19066
IdkMyNaam <127683838+IdkMyNaam@users.noreply.github.com>
2024-12-13 11:07:33 +0100

    Update main.py

--- pythonpart/main.py
+++ pythonpart/main.py
@@ -1,1 +1,22 @@
+import pandas as pd
+from datetime import datetime,timedelta
 
+data = pd.read_excel("data/Hockey_Eerste klasse_tussenstand.xlsx")
+
+overtredingenAantal = data["overtredingen"].count()
+
+SumOvertredingen = data["overtredingen"].sum()
+
+data_stored = data.sort_values("overtredingen", ascending=False)
+top5Overtredingen = data_stored.head(5)
+
+datum = data["datum"]
+datum = pd.to_datetime(data["datum"])
+datum = data["datum"].dt.strftime("%d-%m-%Y")
+today = datetime.now()
+twentyonedaysago = today - timedelta(days=21)
+filter = datum > twentyonedaysago
+
+print(f"aantal overtredding : {overtredingenAantal}")
+print(f"Gemiddeld overtredingen : {SumOvertredingen}")
+print(f"Top 5 overtredingen :\n {top5Overtredingen}")
